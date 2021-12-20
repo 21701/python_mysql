c59a3c17d65bd78f672d73fd353833be4ed5100e
21701 <king2doi@gmail.com>
2021-12-20 10:18:27 +0900

    1

--- test4.py
+++ test4.py
@@ -11,15 +11,29 @@ try :
         password = '4444'
     )
 
-    if connection.is_connected():
-        db_info = connection.get_server_info()
-        print('MySQL info',db_info)
+    qurery = '''select * from test;'''
+
+    cursor = connection.cursor()
+
+    cursor.execute(qurery)
+
+    # select 문은 아래 내용이 필요하다.
+    record_list = cursor.fetchall()
+    print(record_list)
+
+    for row in record_list :
+        print('id =', row[0])
+        print('name =', row[1])
+        print('date =', row[2].isoformat())
+
+
 # 위의 코드를 실행하다가, 문제가 생기면, except를 실행하라는 뜻.
 except Error as e :
     print('Error while connecting to MySQL',e)
 # finally 는 try에서 에러가 나든 안나든, 무조건 실행하라는 뜻.
 finally :
     print('finally')
+    cursor.close()
     if connection.is_connected():
         connection.close()
         print('MySQL connection is closed')
